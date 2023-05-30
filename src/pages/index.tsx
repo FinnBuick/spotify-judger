b2c7bd419e41e07337abcd9956faab97b91bd878
Finn Buick <82785441+FinnUpstreet@users.noreply.github.com>
2023-05-30 22:02:57 +0100

    clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 export default function Home() {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   const router = useRouter();
 
   if (session) {
