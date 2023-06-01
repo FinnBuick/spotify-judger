f248765a70d42e51bd18665efa06ea80fbb25852
Finn Buick <82785441+FinnUpstreet@users.noreply.github.com>
2023-06-01 20:32:45 +0100

    Trigger deploy

--- src/utils/getInitials.ts
+++ src/utils/getInitials.ts
@@ -2,6 +2,8 @@
  * Extract the initials from a name of <= 2 words
  */
 export function getInitials(name: string) {
+  if (!name) return '';
+
   const [firstName, lastName] = name.split(' ');
 
   if (!lastName) return firstName.charAt(0);
