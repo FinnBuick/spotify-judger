4fa9fb94ed035383a0c64dee6cf245416e4422fe
Finn Buick <82785441+FinnUpstreet@users.noreply.github.com>
2023-06-24 17:13:56 +0100

    remove blobs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,12 +32,6 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="blob-cont">
-        <div className="yellow blob"></div>
-        <div className="red blob"></div>
-        <div className="green blob"></div>
-      </div>
-
       <svg>
         <filter id="noiseFilter">
           <feTurbulence type="fractalNoise" baseFrequency="0.6" stitchTiles="stitch" />
