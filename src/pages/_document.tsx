edb6f77d6f75fcda8bc3af7ab4c6f633d7a22e51
Finn Buick <finnbuick@gmail.com>
2023-09-20 20:00:55 +0100

    Add buymeacoffee widget

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          async
+          data-name="BMC-Widget"
+          data-cfasync="false"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="finnbuick"
+          data-description="Support me on Buy me a coffee!"
+          data-message="Hey there, this app costs money to run so if you enjoyed it I would appreciate small donation, if you didn't then let me know and I'll see if I can fix it for you :)"
+          data-color="#5F7FFF"
+          data-position="Right"
+          data-x_margin="18"
+          data-y_margin="18"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
